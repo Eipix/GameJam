33909408928bcd6092ba92372de3855ec6094411
Eipix <64705503+Eipix@users.noreply.github.com>
2025-10-31 21:09:57 +0300

    Add Dotween, OdinInspector

---
+++ Assets/Scripts/Gameplay/Jumping.cs
@@ -0,0 +1,42 @@
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Jumping : SerializedMonoBehaviour
+{
+    [OdinSerialize, ProgressBar(0.01f, 10f)]
+    private float _jumpPower = 1f;
+    [OdinSerialize] private float _duration = 3f;
+
+    private Tween _jumping;
+    private Vector3 _defaultPosition;
+
+    private int _numJumps = 1;
+
+    private void OnValidate()
+    {
+        if (_jumping.IsActive() is false)
+            return;
+
+        Restart();
+    }
+
+    private void Start()
+    {
+        _defaultPosition = transform.position;
+        Restart();
+    }
+
+    [Button]
+    private void Restart()
+    {
+        Kill();
+        transform.position = _defaultPosition;
+        _jumping = transform.DOJump(transform.position, _jumpPower, _numJumps, _duration).SetLoops(-1, LoopType.Restart);
+    }
+
+    [Button]
+    private void Kill() =>_jumping?.Kill();
+}
