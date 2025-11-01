c4c8feb56d22024673c641cf8c60f2fd7da19ff1
shombal1 <sasha.ruchits.06@gmail.com>
2025-11-01 19:04:17 +0300

    Refactor DamageZone to use InvokeRepeating for damage and recreate NavMesh

--- Assets/Scripts/Gameplay/DamageZone.cs
+++ Assets/Scripts/Gameplay/DamageZone.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -14,61 +13,64 @@ namespace Gameplay
         [SerializeField] private float _interval = 1f;
 
         private readonly HashSet<Health> _inside = new HashSet<Health>();
-        private Coroutine _damageCoroutine;
-
-        void Awake()
-        {
-            var col = GetComponent<Collider>();
-            if (col)
-                col.isTrigger = true;
-        }
+        private bool _isDamaging;
 
         void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent<Health>(out var health))
+            {
                 _inside.Add(health);
+                health.OnDie += () => _inside.Remove(health);
+            }
 
-            if (_damageCoroutine == null && _inside.Count > 0)
-                _damageCoroutine = StartCoroutine(DamageLoop());
+            if (!_isDamaging && _inside.Count > 0)
+            {
+                InvokeRepeating(nameof(ApplyDamage), 0f, _interval);
+                _isDamaging = true;
+            }
         }
 
         void OnTriggerExit(Collider other)
         {
-            var health = other.GetComponentInParent<Health>();
-            if (health)
-                _inside.Remove(health);
-
-            if (_inside.Count == 0 && _damageCoroutine != null)
+            if (other.TryGetComponent<Health>(out var health))
             {
-                StopCoroutine(_damageCoroutine);
-                _damageCoroutine = null;
+                _inside.Remove(health);
+            }
+
+            if (_inside.Count == 0 && _isDamaging)
+            {
+                CancelInvoke(nameof(ApplyDamage));
+                _isDamaging = false;
             }
         }
 
-        private IEnumerator DamageLoop()
+        private void ApplyDamage()
         {
-            var wait = new WaitForSeconds(_interval);
-            while (_inside.Count > 0)
+            _inside.RemoveWhere(h => h == null);
+
+            if (_inside.Count == 0)
             {
-                var targets = _inside.ToArray();
-                
-                foreach (var health in targets)
-                {
-                    if (health)
-                        health.TakeDamage(_damage, gameObject);
-                }
-                yield return wait;
+                CancelInvoke(nameof(ApplyDamage));
+                _isDamaging = false;
+                return;
             }
-            _damageCoroutine = null;
+
+            foreach (var health in _inside.ToArray())
+            {
+                Debug.Log(_damage);
+                if (health != null && health.gameObject.activeInHierarchy)
+                    health.TakeDamage(_damage, gameObject);
+            }
         }
 
         void OnDisable()
         {
-            if (_damageCoroutine != null)
+            if (_isDamaging)
             {
-                StopCoroutine(_damageCoroutine);
-                _damageCoroutine = null;
+                CancelInvoke(nameof(ApplyDamage));
+                _isDamaging = false;
             }
+            
             _inside.Clear();
         }
     }
