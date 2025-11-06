65bab234d50b7537933b2df01f82a9d6573b3b88
Eipix <64705503+Eipix@users.noreply.github.com>
2025-11-06 19:40:56 +0300

    Add exit on "esc" click

--- Assets/Scripts/ExitGame.cs
+++ Assets/Scripts/ExitGame.cs
@@ -7,10 +7,8 @@ public class ExitGame : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
 #if UNITY_EDITOR
-            // В редакторе Unity просто останавливаем Play Mode
             UnityEditor.EditorApplication.isPlaying = false;
 #else
-            // В сборке — выходим из приложения
             Application.Quit();
 #endif
         }
